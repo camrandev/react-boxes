58ccd231e97a76c11167962923842fc00cf4310d
Crynowecer0 <crynowecer11@gmail.com>
2023-05-25 14:59:56 -0400

    progress on form: form has client side validation for numbers, working validating colors

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
-import Box from "./Box.js"
+import Box from "./Box.js";
+import NewBoxForm from "./NewBoxForm.js";
+import { v4 as uuid } from 'uuid';
 
 const BoxList = () => {
-  const [,] = useState();
+  //state that contains all of the boxes belongs here
+  //each box will be an object with properties of height, width, color
+  const [boxes, setBoxes] = useState([]);
+
+  function addBox(box) {
+    let newBox = { ...box, id: uuid() };
+    setBoxes(boxes => [...boxes, newBox]);
+  }
 
   return (
     <div>
-      <h3>NewBoxForm</h3>
+      <NewBoxForm addBox={addBox} />
       <Box />
     </div>
   );
---
+++ src/NewBoxForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const NewBoxForm = ({ addBox }) => {
+  const initialState = { height: "", width: "", backgroundColor: "" };
+  const [formData, setFormData] = useState(initialState);
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    addBox(formData);
+    setFormData(initialState);
+  }
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    console.log(evt.target.value);
+    setFormData((formData) => ({
+      ...formData,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="height">Height:</label>
+      <input
+        type="number"
+        min={0} // Set the minimum value
+        max={250} // Set the maximum value
+        id="height"
+        name="height"
+        value={formData.height}
+        onChange={handleChange}
+      />
+      <label htmlFor="width">width:</label>
+      <input
+        type="number"
+        min={0} // Set the minimum value
+        max={250} // Set the maximum value
+        id="width"
+        name="width"
+        value={formData.width}
+        onChange={handleChange}
+      />
+      <label htmlFor="backgroundColor">background color:</label>
+      <input
+        id="backgroundColor"
+        name="backgroundColor"
+        value={formData.backgroundColor}
+        onChange={handleChange}
+      />
+      <button>Add a new box!</button>
+    </form>
+  );
+};
+
+export default NewBoxForm;
